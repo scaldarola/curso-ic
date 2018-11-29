8d541d49d297d6dc8d2826c508d7c8c201d213c2
scaldarola <sebastian.caldarola@mercadolibre.com>
2018-11-29 10:51:59 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker stop curso
 docker rm curso
-docker run --name curso -e --link sql-server:sql-server JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+docker run --name curso --link sql-server:sql-server  -e JAVA_OPTS="-Dspring.profiles.active=ic" -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
